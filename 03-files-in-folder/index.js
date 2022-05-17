8d39012e4ce4eaa1dc2ac7fa4889def01fcf62bd
NailZI <duble76@gmail.com>
2022-05-17 20:43:25 +0300

    feat: complete task 3

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,23 +1,21 @@
-// const fs = require('fs');
-// const path = require('path');
-// const dir = path.join(__dirname + '\\secret-folder');
-// const value = fs.readdir(dir, { withFileTypes: true }, (err, files) => {
-//   if (err) throw err;
-//     // read_files = files;
-//     console.log(files);
-// });
+const fs = require('fs');
+const path = require('path');
+const dir = path.join(__dirname + '\\secret-folder'); // создание пути к папке
+fs.readdir(dir, { withFileTypes: true }, (err, files) => { // извлечение файлов из папки
+  if (err) throw err;
+    files.forEach(element => {
+      let file = {};
+      if (element.isFile()) {
+        file.name = path.basename(element.name, path.extname(element.name));
+        file.extension = path.extname(element.name);
+        path_to_file = path.join(dir + "\\" + element.name);
+        fs.stat(path_to_file, (err, stat) => {
+          // console.log(stat.size);
+          file.size = stat.size;
+          console.log(file.name + ' -' + file.extension.replace(/[.]/g, ' ') + ' - ' + file.size/1000 + ' kb')
+        });
+      }
+    });
+  });
 
-// console.log(value);
-
-const { readdir, stat } = require("fs").promises
-const { join } = require("path")
-
-const dirs = async path => {
-  let dirs = []
-  for (const file of await readdir(path)) {
-    if ((await stat(join(path, file))).isFile()) {
-      dirs = [...dirs, file]
-    }
-  }
-  return dirs
-}
+ 
