7b73f7b04066e549f4325b465eb289872600a7a2
Nail <dubletrain76@gmai.com>
2022-05-19 16:25:16 +0300

    refactor: add comments task 6

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,7 +1,16 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
+const { toUnicode } = require("punycode");
 
-fs.mkdir(path.join(__dirname, 'project-dist'), { recursive: true }, err => {
-    if (err) throw err;
-    // console.log('Папка была создана');
+// Создаёт папку  **project-dist**.
+fs.mkdir(path.join(__dirname, "project-dist"), { recursive: true }, (err) => {
+  if (err) throw err;
+  // console.log('Папка была создана');
 });
+
+// Заменяет шаблонные теги в файле **template.html** с названиями файлов из папки components (пример:```{{section}}```) на содержимое одноимённых компонентов и  сохраняет результат в **project-dist/index.html**.
+
+
+// Собирает в единый файл стили из папки **styles** и помещает их в файл **project-dist/style.css**.
+
+// Копирует папку **assets** в **project-dist/assets**
