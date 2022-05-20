caa9375ad3e2121ab95b570449f47bbb2393e657
Nail <dubletrain76@gmai.com>
2022-05-20 16:17:49 +0300

    feat: task 6 add assets

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,6 +1,5 @@
 const fs = require("fs");
 const path = require("path");
-// const { toUnicode } = require("punycode");
 
 // Создаёт папку  **project-dist**.
 fs.mkdir(path.join(__dirname, "project-dist"), { recursive: true }, (err) => {
@@ -9,11 +8,17 @@ fs.mkdir(path.join(__dirname, "project-dist"), { recursive: true }, (err) => {
 });
 
 // Заменяет шаблонные теги в файле **template.html** с названиями файлов из папки components (пример:```{{section}}```) на содержимое одноимённых компонентов и  сохраняет результат в **project-dist/index.html**.
-// const dir_components = path.join(__dirname, 'components');
-// fs.readFile(dir_components, (err, files) => {
-//   if (err) throw err;
-//   console.log(files)
-// });
+const dir_components = path.join(__dirname, 'components');
+let comp_files = [];
+const any = await fs.readdir(dir_components, (err, files) => {
+    if (err) throw err;
+    files.forEach(element => {
+      comp_files.join(element);
+      console.log(element);
+    })
+  });
+// while (!comp_files.length) {}
+console.log('components> ', comp_files)
 // const read_file = path.join(__dirname, 'template.html');
 // const write_file = fs.createWriteStream(path.join(__dirname, '\\project-dist\\index.html'));
 // fs.readFile(read_file, (err, data) => {
@@ -50,51 +55,29 @@ fs.readdir(dir, { withFileTypes: true }, (err, files) => { // извлечени
 });
 
 // Копирует папку **assets** в **project-dist/assets**
-let dir_in = 'assets';
-let dir_out = 'project-dist';
-let path_dir_in = path.join(__dirname, `\\${dir_in}`);
-let path_dir_out = path.join(__dirname, `\\${dir_out}`);
-function copy_files(dir_in, dir_out, path_dir_in, path_dir_out) {
-  // console.log(dir_in, '\n', dir_out, '\n', path_dir_in, '\n', path_dir_out)
-  fs.mkdir(path.join(path_dir_out, dir_in), { recursive: true }, (err) => {
-    if (err) throw err;
-    });
+const dir_name = 'assets'; // папка из которой копируем
+fs.mkdir(path.join(__dirname, 'project-dist', dir_name), { recursive: true }, (err) => { // создаем папку assets  в project-dist
+  if (err) throw err;
+  });
+// функция читает содержимое папки и переносит файлы в project-dist
+function copy_files(dir_name) {
+  let path_dir_in = path.join(__dirname, dir_name); // путь к папке из которой берем
+  let path_dir_out = path.join(__dirname, 'project-dist', dir_name); // путь к папке в которую кладем
   fs.readdir(path_dir_in, { withFileTypes: true }, (err, files) => {
     if (err) throw err;
     files.forEach(element => {
-      // console.log(element)
-      if (element.isFile()) {
-        console.log('file> ', element)
-        // fs.copyFile(path_dir_in, path_dir_out, err => {
-        //   if (err) throw err;
-        // })
-      } else {
-          console.log('dir> ', element)
-          dir_in = element.name;
-          dir_out = element.name;
-          console.log('dir_in> ', dir_in);
-          console.log('dir_out> ', dir_out);
-          
-          // path_dir_in += '\\' + element.name;
-          // path_dir_out += '\\' + element.name;
-          path_dir_in = path.join(__dirname, element.name);
-          path_dir_out = path.join(__dirname, element.name);
-          copy_files(dir_in, dir_out, path_dir_in, path_dir_out);
+      if (!element.isFile()) { // копируем вложенные папки и читаем их содержимое
+        fs.mkdir(path.join(path_dir_out, element.name), { recursive: true }, (err) => {
+          if (err) throw err;
+          });
+        let internal_dir = dir_name + '\\' + element.name;
+        return copy_files(internal_dir); // рекурсивный запуск функции
+      } else if (element.isFile()) { // копируем файлы
+          fs.copyFile(path.join(path_dir_in, element.name), path.join(path_dir_out, element.name), err => {
+          if (err) throw err;
+        })
       }
     })
   });
 }
-copy_files(dir_in, dir_out, path_dir_in, path_dir_out)
-// fs.mkdir(path.join(dir_out, 'assets'), { recursive: true }, (err) => {
-//   if (err) throw err;
-//   // console.log('Папка была создана');
-// });
-// fs.readdir(dir_in, (err, files) => { // извлечение файлов из папки
-//     if (err) throw err;
-//       files.forEach(element => {
-//         fs.copyFile(path.join(dir_in, element), path.join(dir_out, 'assets\\', element), err => {
-//             if (err) throw err;
-//             // console.log(element);
-//         })
-//       });
-//     });
+copy_files(dir_name)
