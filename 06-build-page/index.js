054723742e23ad4cc007ddd7696e7bce02ebf624
NailZI <duble76@gmail.com>
2022-05-20 20:59:10 +0300

    feat: complete task 6

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const promises = require("fs/promises")
 const path = require("path");
 
 // Создаёт папку  **project-dist**.
@@ -8,51 +9,45 @@ fs.mkdir(path.join(__dirname, "project-dist"), { recursive: true }, (err) => {
 });
 
 // Заменяет шаблонные теги в файле **template.html** с названиями файлов из папки components (пример:```{{section}}```) на содержимое одноимённых компонентов и  сохраняет результат в **project-dist/index.html**.
-const dir_components = path.join(__dirname, 'components');
-let comp_files = [];
-const any = await fs.readdir(dir_components, (err, files) => {
-    if (err) throw err;
-    files.forEach(element => {
-      comp_files.join(element);
-      console.log(element);
-    })
-  });
-// while (!comp_files.length) {}
-console.log('components> ', comp_files)
-// const read_file = path.join(__dirname, 'template.html');
-// const write_file = fs.createWriteStream(path.join(__dirname, '\\project-dist\\index.html'));
-// fs.readFile(read_file, (err, data) => {
-//   if(err) throw err;
-//   let array = data.toString().split("\n");
-//   array.forEach(element => {
-//       console.log(element);
-//       if (element.includes('first')) {
-//           console.log('первая строка');
-//           write_file.write('11111');
-//           write_file.write('\n');
-//       } else {
-//           write_file.write(element);
-//           // fs.createWriteStream(path.join(__dirname, 'new.txt')).write("\n");
-//       }
-//   }); 
-// });
+async function merge_html() {
+  const dir_components = path.join(__dirname, 'components');
+  let comp_files = [];
+  const any_file = await promises.readdir(dir_components, { withFileTypes: true });
+  for (const files of any_file) {
+    let obj = {};
+    if (files.isFile() && path.extname(files.name) === '.html') {
+      obj.name = path.basename(files.name, path.extname(files.name));
+      obj.content = await promises.readFile(path.join(dir_components, files.name), 'utf-8');
+      comp_files.push(obj)
+    }
+  }
+  const path_to_read_file = path.join(__dirname, 'template.html');
+  const write_file = fs.createWriteStream(path.join(__dirname, '\\project-dist\\index.html'));
+  let read_temp = await promises.readFile(path_to_read_file, 'utf-8');
+  for (const part of comp_files) {
+    read_temp = read_temp.replace(`{{${part.name}}}`, part.content);
+  }
+  write_file.write(read_temp); // заполняем index.html
+}
+merge_html();
 
 // Собирает в единый файл стили из папки **styles** и помещает их в файл **project-dist/style.css**.
-const dir = path.join(__dirname + '\\styles'); // создание пути к папке
-fs.readdir(dir, { withFileTypes: true }, (err, files) => { // извлечение файлов из папки
-  if (err) throw err;
-  files.forEach(element => {
-    if (element.isFile() && path.extname(element.name) === '.css') {
-      let content = fs.createReadStream(path.join(__dirname, '\\styles\\', element.name), 'utf-8');
-      content.on('data', chunk => {
-        fs.appendFile(path.join(__dirname, '\\project-dist\\', 'style.css'), chunk, err => {
-          if (err) throw err;
-          // console.log('OK')
-        })
-      });
+async function merge_css() {
+  const dir = path.join(__dirname + '\\styles'); // создание пути к папке
+  const read_styles = await promises.readdir(dir, { withFileTypes: true });
+  for (const style_file of read_styles) {
+      if (style_file.isFile() && path.extname(style_file.name) === '.css') {
+        let content = fs.createReadStream(path.join(__dirname, '\\styles\\', style_file.name), 'utf-8');
+        content.on('data', chunk => {
+          fs.appendFile(path.join(__dirname, '\\project-dist\\', 'style.css'), chunk, err => {
+            if (err) throw err;
+            // console.log('OK')
+          })
+        });
+      }
     }
-  })
-});
+};
+merge_css();
 
 // Копирует папку **assets** в **project-dist/assets**
 const dir_name = 'assets'; // папка из которой копируем
