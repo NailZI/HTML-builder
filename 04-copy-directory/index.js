984f3ef4b78d71b0899951d3a58595d24c18ee36
NailZI <duble76@gmail.com>
2022-05-26 20:19:18 +0300

    fix: add clean new folder task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -7,12 +7,20 @@ fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, err => {
 const dir_in = path.join(__dirname, '\\files');
 const dir_out = path.join(__dirname, '\\files-copy');
 // fs.readdir(dir_in, { withFileTypes: true }, (err, files) => { // извлечение файлов из папки
+fs.readdir(dir_out, (err, files) => {
+  if (err) throw err;
+  files.forEach(element => {
+    fs.unlink(path.join(dir_out, element), err => {
+      if (err) throw err;
+    });
+  })
+})
 fs.readdir(dir_in, (err, files) => { // извлечение файлов из папки
     if (err) throw err;
-      files.forEach(element => {
-        fs.copyFile(path.join(dir_in, element), path.join(dir_out, element), err => {
-            if (err) throw err;
-            // console.log(element);
-        })
-      });
+    files.forEach(element => {
+      fs.copyFile(path.join(dir_in, element), path.join(dir_out, element), err => {
+          if (err) throw err;
+          // console.log(element);
+      })
+    });
     });
